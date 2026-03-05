8073596ca8832a855f80878ec7d3987535357485
Andy Thai <andy.thai2004@gmail.com>
2026-03-05 15:47:12 -0800

    Foundation

---
+++ packages/core/src/groupController.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Study Groups!\n";
+    return 0;
+}
---
+++ packages/core/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Main!\n";
+    return 0;
+}
